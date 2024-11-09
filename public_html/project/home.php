7378bc63114d532d72233b8bf358eef1eeaaeb45
Benzi <benzi.villaruel@gmail.com>
2024-11-08 22:11:54 -0500

    Fix small issues

--- public_html/project/home.php
+++ public_html/project/home.php
@@ -3,6 +3,7 @@ require(__DIR__."/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <?php
+error_log("SESSION DATA: " . var_export($_SESSION, true));
 if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
  echo "Welcome, " . $_SESSION["user"]["email"]; 
 }
