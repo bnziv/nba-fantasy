9acc4a332ebd3ed3a379329ee268040416341dda
Benzi <benzi.villaruel@gmail.com>
2024-11-10 00:42:10 -0500

    Add user roles tables

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -29,35 +29,28 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
     //TODO 3
     $hasError = false;
     if (empty($email)) {
-        flash("Email must not be empty");
+        flash("Email must not be empty", "danger");
         $hasError = true;
     }
     //sanitize
-    //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
     $email = sanitize_email($email);
     //validate
-    /*if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        flash("Invalid email address");
-        $hasError = true;
-    }*/
     if (!is_valid_email($email)) {
-        flash("Invalid email address");
+        flash("Invalid email address", "danger");
         $hasError = true;
     }
     if (empty($password)) {
-        flash("Password must not be empty");
+        flash("password must not be empty", "danger");
         $hasError = true;
     }
-    if (!is_valid_password($password)) {
-        flash("Password too short");
+    if (strlen($password) < 8) {
+        flash("Password too short", "danger");
         $hasError = true;
     }
     if (!$hasError) {
-        //flash("Welcome, $email");
         //TODO 4
         $db = getDB();
-        $stmt = $db->prepare("SELECT id, email, username, password from Users 
-        where email = :email");
+        $stmt = $db->prepare("SELECT id, email, username, password from Users where email = :email");
         try {
             $r = $stmt->execute([":email" => $email]);
             if ($r) {
@@ -66,15 +59,14 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                     $hash = $user["password"];
                     unset($user["password"]);
                     if (password_verify($password, $hash)) {
-                        //flash("Weclome $email");
-                        $_SESSION["user"] = $user; //sets our session data from db
-                        flash("Welcome, " . get_username());
+                        flash("Welcome $email");
+                        $_SESSION["user"] = $user;
                         die(header("Location: home.php"));
                     } else {
-                        flash("Invalid password");
+                        flash("Invalid password", "danger");
                     }
                 } else {
-                    flash("Email not found");
+                    flash("Email not found", "danger");
                 }
             }
         } catch (Exception $e) {
@@ -83,5 +75,6 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
     }
 }
 ?>
-<?php 
-require(__DIR__."/../../partials/flash.php");
+<?php
+require(__DIR__ . "/../../partials/flash.php");
+?>
